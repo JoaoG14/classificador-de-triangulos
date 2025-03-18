400a663afbd3ef122e51f494945d6dbb6124ea85
João <joaog.alves14@gmail.com>
2025-03-18 17:14:17 -0300

    Adicionada classe estática ClassificadorDeTriangulos

---
+++ ClassifiadorDeTriangulos.ConsoleApp/ClassificadorDeTriangulos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassificadorDeTriangulos.ConsoleApp
+{
+    public class ClassificadorDeTriangulos
+    {
+        public int LadoX { get; set; }
+        public int LadoY { get; set; }
+        public int LadoZ { get; set; }
+
+        public ClassificadorDeTriangulos(int ladoX, int ladoY, int ladoZ)
+        {
+            LadoX = ladoX;
+            LadoY = ladoY;
+            LadoZ = ladoZ;
+        }
+
+        public bool EhTriangulo()
+        {
+            return (LadoX + LadoY > LadoZ) &&
+                   (LadoX + LadoZ > LadoY) &&
+                   (LadoY + LadoZ > LadoX);
+        }
+
+        public string Classificar()
+        {
+            if (!EhTriangulo())
+                return "Não é um Triângulo";
+
+            if (LadoX == LadoY && LadoY == LadoZ)
+                return "Triângulo Equilátero";
+
+            if (LadoX == LadoY || LadoX == LadoZ || LadoY == LadoZ)
+                return "Triângulo Isósceles";
+
+            return "Triângulo Escaleno";
+        }
+    }
+}
--- ClassifiadorDeTriangulos.ConsoleApp/Program.cs
+++ ClassifiadorDeTriangulos.ConsoleApp/Program.cs
@@ -1,82 +1,68 @@
-﻿namespace ClassifiadorDeTriangulos.ConsoleApp
+﻿namespace ClassificadorDeTriangulos.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-            while(true)
+            while (true)
             {
-
                 Console.Clear();
-                Console.WriteLine("------------------------------------");
-                Console.WriteLine("Classificador de Triângulos");
-                Console.WriteLine("------------------------------------");
+                ExibirCabecalho();
 
-                int ladoX;
-                int ladoY;
-                int ladoZ;
+                int ladoX = LerLado("X");
+                int ladoY = LerLado("Y");
+                int ladoZ = LerLado("Z");
 
-                while (true)
-                {
-                    Console.Write("Digite o valor do lado X: ");
-                    bool conseguiuConverter = int.TryParse(Console.ReadLine(), out ladoX);
-                    if (conseguiuConverter)
-                        break;
-                }
+                ExibirValoresInformados(ladoX, ladoY, ladoZ);
 
-                while (true)
-                {
-                    Console.Write("Digite o valor do lado Y: ");
-                    bool conseguiuConverter = int.TryParse(Console.ReadLine(), out ladoY);
-                    if (conseguiuConverter)
-                        break;
-                }
+                // Criar uma instância da classe ClassificadorDeTriangulos
+                ClassificadorDeTriangulos classificador = new ClassificadorDeTriangulos(ladoX, ladoY, ladoZ);
 
-                while (true)
-                {
-                    Console.Write("Digite o valor do lado Z: ");
-                    bool conseguiuConverter = int.TryParse(Console.ReadLine(), out ladoZ);
-                    if (conseguiuConverter)
-                        break;
-                }
-
-
-                Console.WriteLine("------------------------------------");
-                Console.WriteLine("Valores Informados:");
-                Console.WriteLine($"Lado X: {ladoX}");
-                Console.WriteLine($"Lado Y: {ladoY}");
-                Console.WriteLine($"Lado Z: {ladoZ}");
+                // Obter a classificação e exibir o resultado
+                string resultado = classificador.Classificar();
+                Console.WriteLine(resultado);
                 Console.WriteLine("------------------------------------");
 
-
-                if (ladoX + ladoY < ladoZ || ladoX + ladoZ < ladoY || ladoY + ladoZ < ladoX)
-                {
-                    Console.WriteLine("É um Triângulo");
-                }
-                else if (ladoX == ladoY && ladoY == ladoZ)
-                {
-                    Console.WriteLine("Triângulo Equilátero");
-                }
-                else if (ladoX == ladoY || ladoX == ladoZ || ladoY == ladoZ)
-                {
-                    Console.WriteLine("Triângulo Isósceles");
-                }
-                else
-                {
-                    Console.WriteLine("Não é um Triângulo");
-                }
-
-
-                Console.WriteLine("------------------------------------");
-                Console.WriteLine("Deseja continuar? (S/N) ");
-                string continuar = Console.ReadLine()!.ToUpper();
-
-
-                if (continuar == "N")
+                if (!DesejaContinar())
                     break;
             }
-            
+        }
+
+        static void ExibirCabecalho()
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Classificador de Triângulos");
+            Console.WriteLine("------------------------------------");
+        }
+
+        static int LerLado(string nomeLado)
+        {
+            int lado;
+            while (true)
+            {
+                Console.Write($"Digite o valor do lado {nomeLado}: ");
+                if (int.TryParse(Console.ReadLine(), out lado))
+                    return lado;
+
+                Console.WriteLine("Valor inválido. Digite um número inteiro.");
+            }
+        }
+
+        static void ExibirValoresInformados(int ladoX, int ladoY, int ladoZ)
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Valores Informados:");
+            Console.WriteLine($"Lado X: {ladoX}");
+            Console.WriteLine($"Lado Y: {ladoY}");
+            Console.WriteLine($"Lado Z: {ladoZ}");
+            Console.WriteLine("------------------------------------");
+        }
+
+        static bool DesejaContinar()
+        {
+            Console.WriteLine("Deseja continuar? (S/N) ");
+            string continuar = Console.ReadLine()?.ToUpper() ?? "N";
+            return continuar == "S";
         }
     }
 }
